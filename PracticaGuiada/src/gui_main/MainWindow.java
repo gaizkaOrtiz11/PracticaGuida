43d228c454c1bbe3424223c28290b286a5c427c5
Gaizka Ortiz <gaizka.ortiz@opendeusto.es>
2024-10-20 20:44:51 +0200

    Segundo TErcer ejercicio

--- PracticaGuiada/src/gui_main/MainWindow.java
+++ PracticaGuiada/src/gui_main/MainWindow.java
@@ -1,13 +1,16 @@
 package gui_main;
 
 import java.awt.BorderLayout;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.*;
 
 public class MainWindow extends JFrame {
 	public MainWindow() {
@@ -16,7 +19,7 @@ public class MainWindow extends JFrame {
 		setSize(640,480);
 		setLocationRelativeTo(null);
 		
-		String[] lista= {"Juan Salcedo","David Cordero","Ibai Barrios","Luken Menendez","Guillermo Cordero"};
+		String[] lista= {"Juan Salcedo","Borja Hernandez","Ibai Barrios","Luken Menendez","Guillermo Cordero"};
 		JList<String> jList = new JList<String>(lista);
 		JScrollPane jScrollPane = new JScrollPane(jList);
 		add(jScrollPane,BorderLayout.WEST);
@@ -26,9 +29,28 @@ public class MainWindow extends JFrame {
 		jTabbedPane.addTab("Medallas", new JPanel());
 		add(jTabbedPane,BorderLayout.CENTER);
 		
+		JMenuBar barra = new JMenuBar();
+		setJMenuBar(barra);
 		
-		JMenuBar menubar = new JMenuBar();
+		JMenu menufichero = new JMenu("Fichero");
+		barra.add(menufichero);
+        menufichero.setMnemonic(KeyEvent.VK_F);
+
+		JMenuItem OpcionNuevoAtleta = new JMenuItem("Nuevo Atleta");
+        menufichero.add(OpcionNuevoAtleta);
+        OpcionNuevoAtleta.setMnemonic(KeyEvent.VK_N);
 		
+        menufichero.addSeparator();
+        
+		JMenuItem OpcionImportar = new JMenuItem("Importar");
+        menufichero.add(OpcionImportar);
+        OpcionImportar.setMnemonic(KeyEvent.VK_I);
+        
+        JMenuItem OpcionExportar = new JMenuItem("Exportar");
+        menufichero.add(OpcionExportar);
+        OpcionExportar.setMnemonic(KeyEvent.VK_E);
+		
+        
 
 		
 		setVisible(true);
